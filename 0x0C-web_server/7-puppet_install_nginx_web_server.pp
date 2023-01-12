0bae0c188c425f2b2566eb00ce418a9764e66db7
lukwagoraymond <lukwagoraymond3@gmail.com>
2023-01-12 21:53:46 +0300

    Corrected Polkit authentication Error

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -11,15 +11,15 @@ exec {'install':
 }
 
 exec {'Hello':
-  command  => 'echo "Hello World!" > /var/www/html/index.html',
+  command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
   provider => shell,
 }
 
-exec {'sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/www.youtube.com/watch?v=QH2-TGUlwu4\/;\\n\\t}/" /etc/nginx/sites-available/default':
+exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/www.youtube.com/watch?v=QH2-TGUlwu4\/;\\n\\t}/" /etc/nginx/sites-available/default':
   provider => shell,
 }
 
 exec {'run':
-  command  => 'service nginx restart',
+  command  => 'sudo service nginx restart',
   provider => shell,
 }
