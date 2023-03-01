005982c2a4a33676b6a17ae02f263381ac21b7c3
Raymond Lukwago <lukwagoraymond3@gmail.com>
2023-03-01 14:49:20 +0300

    Included code for checking subreddit validity

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,13 +4,15 @@
     for a given subreddit.
 """
 import requests
-from sys import argv
 
 
 def number_of_subscribers(subreddit):
     """Get the number of subscribers in subreddit"""
     base_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'TestAPI/0.0.1'}
+
+    if type(subreddit) is not str or subreddit is None:
+        return 0
     response = requests.get(base_url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         return 0
