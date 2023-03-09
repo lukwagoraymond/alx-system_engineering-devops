e4e35fe8e23bcefc04c6b7b11ef45ba156bb2bdb
Raymond Lukwago <lukwagoraymond3@gmail.com>
2023-03-09 16:59:40 +0300

    Corrected sed command

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,14 +1,14 @@
 # Increases the amount of traffic an Nginx server can handle
 
 # Increase the ULIMIT of the default file
-exec { 'fix--for-nginx':
-    command  => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 5000\"/g" /etc/default/nginx,
-    provider => shell,
+exec { 'ulimit':
+  command  => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 5000\"/g" /etc/default/nginx',
+  provider => shell,
 }
 
 # Restart Nginx
 exec { 'restorenginx':
-    command  => 'service nginx restart',
-    provider => shell,
-    require  => Exec['fix--for-nginx'],
+  command  => 'service nginx restart',
+  provider => shell,
+  require  => Exec['ulimit'],
 }
